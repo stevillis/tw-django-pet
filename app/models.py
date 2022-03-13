af6540e5dc94331930bf0eb68d50ed3fc0173737
Stévillis Sousa <stevillis@hotmail.com>
2022-03-13 02:00:02 -0400

    Create Models: Endereco and Cliente

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from localflavor.br.br_states import STATE_CHOICES
 
-# Create your models here.
+
+class Endereco(models.Model):
+    rua = models.CharField(max_length=60, null=False, blank=False)
+    cidade = models.CharField(max_length=60, null=False, blank=False)
+    estado = models.CharField(max_length=2, choices=STATE_CHOICES)
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, related_name='clientes')
+    cpf = models.CharField(max_length=14, null=False, blank=False)
+    data_nascimento = models.DateField(null=False, blank=False)
+    profissao = models.CharField(max_length=60, null=False, blank=False)
